78ffdf10debc5b43a7b498774443a14b5c735c31
ramuiruri <rayjaymuiruri@gmail.com>
2024-06-09 13:40:55 +0300

    arrays

--- array.sh
+++ array.sh
@@ -4,8 +4,9 @@ arr=('apple' 'mango' 'banana')
 #number of elements
 echo "${#arr[@]}"
 length_of_array="${#arr[@]}"
+
 #add an element into the 'arr' array
-arr[length_of_array]='onions'
+arr[length_of_array]='onions' #this will add an element at index 3 since the length of the array is 3 now before appending onions to the array.
 echo "${arr[@]}"
 
 #print the indexes
